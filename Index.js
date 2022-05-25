07e4d32f2055a78b22312cf364685ed63291f4c5
Awais <Awaisbinmohsin@gmail.com>
2022-05-26 00:30:29 +0500

    Booleans & Comparisons

---
+++ Index.js
@@ -0,0 +1,62 @@
+
+        let email = "Awaisbinmohsin@gmail.com";
+        // let result = email.includes("@");
+
+        let result = email.includes("!");
+        console.log(result);
+
+       // Comparison for true and false
+
+
+        let age = 25;
+        console.log(age==25);
+        console.log(age!=25);
+        console.log(age!=30);
+        console.log(age>20);
+        console.log(age>30);
+        console.log(age<30);
+        console.log(age<20);
+
+
+
+
+        let name = "James"
+        console.log(name=="James");
+
+
+
+        // Case 1 = false
+
+        let name1 = "James";
+        console.log(name1=="james");
+
+        // Case 2 = false
+
+        let name2 = "james";
+        console.log(name2=="James");
+
+        // Case 3 = false
+
+        let name3 = "jhon";
+        console.log(name3>"smith");
+
+        // Case 4 = true
+
+        let name4 = "jhon";
+        console.log(name4>"Smith");
+
+        // Case 5 = false
+
+        let name5= "Jhon";
+        console.log(name5>"Smith");
+
+        // Case 6 = false
+
+        let name6 = "jhon";
+        console.log(name6>"smith");
+
+        // case 7 = ture
+
+        let name7 = "jhon";
+        console.log(name7>"Jhon");
+
